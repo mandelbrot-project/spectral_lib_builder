7db5d3d928e04f3e83fbe806138ce96540baf948
Adafede <adriano.rutz@hotmail.com>
2022-04-04 11:05:53 +0200

    update

--- scripts/log2mgf.py
+++ scripts/log2mgf.py
@@ -17,6 +17,7 @@ import sys
 try:
     directory_name=sys.argv[1]
     extension_type=sys.argv[2]
+    manipulation=sys.argv[3]
     print('Parsing directory' + directory_name + ' with file extension :' + extension_type)
 except:
     print('Please pass parent directory, where the .log files are placed')
@@ -31,7 +32,7 @@ p = re.compile(r'^^([0-9]*\.{1}[0-9]*) ([0-9]*\.{1}[0-9]*)', re.M)
 for filename in glob.iglob(str(directory_name) + '*' + str(extension_type), recursive=True):
     
     try:
-        #we then get a first block corresponding to masses only
+        # we then get a first block corresponding to masses only
         masses = open(filename,'r').read().split('\n\n')
         
         # and split this block according to te energy pattern
@@ -43,14 +44,25 @@ for filename in glob.iglob(str(directory_name) + '*' + str(extension_type), recu
         list_energy_all = list_energy_all.append(pd.DataFrame(re.findall(p, split[3])))
         list_energy_all.rename(columns={0:'mass'},inplace=True)
         list_energy_all['mass'] = pd.to_numeric(list_energy_all['mass'], errors='coerce')
-        list_energy_all = list_energy_all.sort_values('mass', ascending = True)
+        list_energy_all = list_energy_all.astype(float)
+
+        # manipulation
+        if len(sys.argv) > 3:
+            # must be one of ['mean','max','sum']
+            list_energy_all_manipulated = list_energy_all.groupby(['mass']).eval(sys.argv[3])()
+        else:
+            list_energy_all_manipulated = list_energy_all['mass']
+
+        list_energy_all_manipulated = list_energy_all_manipulated.reset_index()
+
+        list_energy_all = list_energy_all_manipulated.sort_values('mass', ascending = True)
     
-        # uncomment if you want to output to a differnt filename
+        # uncomment if you want to output to a different filename
         mgf_filename = filename.split('.log')[0]+'.mgf'
         
-        # be carefull here we'll crush the original files
         list_energy_all.to_csv(mgf_filename,sep=' ', index=False, header=False)
-        os.remove(filename)
+        # be carefull if you remove the original files
+        # os.remove(filename)
         done += 1
         
     except Exception as er:
